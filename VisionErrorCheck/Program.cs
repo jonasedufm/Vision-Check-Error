d25302458efec94ce2dcc6e151e5a1c2bd66d31b
Jonas Moraes <Jonas.Moraes@flex.com>
2023-06-28 14:17:54 -0300

    First commit

---
+++ VisionErrorCheck/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace VisionErrorCheck
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                iniFile MyIni = new iniFile("Erros.ini");
+                string DetalheErro = string.Empty;
+
+                Console.Title = "Vision Check Error";
+                Console.WriteLine("Digite o numero do erro: ");
+                int erro = int.Parse(Console.ReadLine());
+
+                if (MyIni.keyExist(erro.ToString(), "ListaErros"))
+                    DetalheErro = MyIni.read(erro.ToString(), "ListaErros");
+
+                else
+                {
+                    Console.WriteLine("Não foi encontrado o erro!!");
+                }
+
+                Console.WriteLine(DetalheErro);
+            }
+            catch
+            {
+                Console.WriteLine("Digite um número válido.");
+            }
+        }
+    }
+}
---
+++ VisionErrorCheck/iniFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.IO;
+
+namespace VisionErrorCheck
+{
+    class iniFile
+    {
+        string Path;
+        string fileName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public iniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? fileName + ".ini").FullName;
+        }
+
+        public string read(string key, string section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(section ?? fileName, key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+
+        public bool keyExist(string key, string section = null)
+        {
+            return read(key, section).Length > 0;
+        }
+    }
+}
